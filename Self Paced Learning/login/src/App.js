8c95c187e5839a7d93d86c7e47c54452f3a020bf
taofikarianto <taofikarianto@gmail.com>
2022-10-10 00:30:27 +0700

    TaofikArianto-025-React

---
+++ Self Paced Learning/login/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { useSelector } from "react-redux";
+import "./App.css";
+import Login from "./components/Login";
+import Logout from "./components/Logout";
+import { selectUser } from "./features/userSlice";
+
+function App() {
+  const user = useSelector(selectUser);
+  return <div>{user ? <Logout /> : <Login />}</div>;
+}
+
+export default App;
